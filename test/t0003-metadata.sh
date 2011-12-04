5b3b363cd23c4df2bccc07c417159392a4ed90f3
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2011-12-04 20:41:37 +0100

    Add metadata() helper to test

--- test/t0003-metadata.sh
+++ test/t0003-metadata.sh
@@ -4,18 +4,27 @@ test_description="Get metadata of shortened links"
 
 . ./sharness.sh
 
-url="http://www.paulgraham.com/gh.html"
-rdd_id="ga4qf47t"
+json_str() {
+    test -n "$1" && echo -n \"$1\" || echo -n null
+}
 
-cat >expect <<EOF
+metadata() {
+    url="$1"
+    title=$(json_str "$2")
+    excerpt=$(json_str "$3")
+    words="$4"
+    author=$(json_str "$5")
+    rdd_id="$6"
+
+    cat >expect <<EOF
 {
     "meta": {
         "article": {
             "url": "$url", 
-            "title": "Great Hackers", 
-            "excerpt": "Want to start a startup? Get funded by Y Combinator . July 2004 (This essay is derived from a talk at Oscon 2004.) A few months ago I finished a new book , and in reviews I keep noticing words like&hellip;", 
-            "word_count": 5147, 
-            "author": null
+            "title": $title, 
+            "excerpt": $excerpt, 
+            "word_count": $words, 
+            "author": $author
         }, 
         "rdd_url": "http://rdd.me/$rdd_id", 
         "id": "$rdd_id", 
@@ -28,69 +37,31 @@ cat >expect <<EOF
 }
 EOF
 
-test_expect_success "Get metadata of $rdd_id ($url)" "
-    rdd_r metadata $rdd_id >result &&
-    test_cmp expect result
-"
-
-
-url="http://the99percent.com/articles/6943/What-Motivates-Us-To-Do-Great-Work"
-rdd_id="75x8oaqg"
-
-cat >expect <<EOF
-{
-    "meta": {
-        "article": {
-            "url": "$url", 
-            "title": "What Motivates Us To Do Great Work? :", 
-            "excerpt": "What motivates us to do great work? It s an age-old question. But the age-old answers rewards, recognition, money, stability no longer seem to suffice. As we ve shifted to a knowledge-based economy,&hellip;", 
-            "word_count": 667, 
-            "author": "Jocelyn K. Glei"
-        }, 
-        "rdd_url": "http://rdd.me/$rdd_id", 
-        "id": "$rdd_id", 
-        "full_url": "http://readability.com/articles/$rdd_id"
-    }, 
-    "messages": [
-        "Article found."
-    ], 
-    "success": true
+    test_expect_success "Get metadata of $rdd_id ($url)" "
+        rdd_r metadata $rdd_id >result &&
+        test_cmp expect result
+    "
 }
-EOF
 
-test_expect_success "Get metadata of $rdd_id ($url)" "
-    rdd_r metadata $rdd_id >result &&
-    test_cmp expect result
-"
+metadata "http://www.paulgraham.com/gh.html" \
+    "Great Hackers" \
+    "Want to start a startup? Get funded by Y Combinator . July 2004 (This essay is derived from a talk at Oscon 2004.) A few months ago I finished a new book , and in reviews I keep noticing words like&hellip;" \
+    5147 \
+    "" \
+    ga4qf47t
 
+metadata "http://the99percent.com/articles/6943/What-Motivates-Us-To-Do-Great-Work" \
+    "What Motivates Us To Do Great Work? :" \
+    "What motivates us to do great work? It s an age-old question. But the age-old answers rewards, recognition, money, stability no longer seem to suffice. As we ve shifted to a knowledge-based economy,&hellip;" \
+    667 \
+    "Jocelyn K. Glei" \
+    75x8oaqg
 
-url="http://www.inc.com/magazine/20100401/driven-to-distraction.html"
-rdd_id="yibs1cca"
-
-cat >expect <<EOF
-{
-    "meta": {
-        "article": {
-            "url": "$url", 
-            "title": "Driven to Distraction", 
-            "excerpt": "Are your business problems making you insane? In his debut column, 37signals co-founder Jason Fried argues that one of the keys to success is to let your lazy side guide you. I think of myself as&hellip;", 
-            "word_count": 884, 
-            "author": "Jason Fried"
-        }, 
-        "rdd_url": "http://rdd.me/$rdd_id", 
-        "id": "$rdd_id", 
-        "full_url": "http://readability.com/articles/$rdd_id"
-    }, 
-    "messages": [
-        "Article found."
-    ], 
-    "success": true
-}
-EOF
-
-test_expect_success "Get metadata of $rdd_id ($url)" "
-    rdd_r metadata $rdd_id >result &&
-    test_cmp expect result
-"
+metadata "http://www.inc.com/magazine/20100401/driven-to-distraction.html" \
+    "Driven to Distraction" \
+    "Are your business problems making you insane? In his debut column, 37signals co-founder Jason Fried argues that one of the keys to success is to let your lazy side guide you. I think of myself as&hellip;" \
+    884 \
+    "Jason Fried" \
+    yibs1cca
 
 test_done
