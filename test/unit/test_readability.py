aba86d8eead5b9fd7097e16165e631d4cb229670
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2012-11-01 13:37:49 +0100

    Add test fixtures

--- test/unit/test_readability.py
+++ test/unit/test_readability.py
@@ -1,8 +1,15 @@
 # -*- coding: utf-8 -*-
 
-import pytest
+import json
 import mock
+import os
+import pytest
 import rdd
+
+
+def load_fixture(filename):
+    path = os.path.join(os.path.dirname(__file__), 'fixtures', filename)
+    return json.load(open(path))
 
 
 class TestReadability(object):
@@ -13,24 +20,25 @@ class TestReadability(object):
         self.readability._request = self.request
 
     def test_resources(self):
-        resources = {} # TODO
-        self.request.return_value = {'resources': resources}
-        assert self.readability.resources() == resources
+        json = load_fixture('resources.json')
+        self.request.return_value = json
+        assert self.readability.resources() == json['resources']
         self.request.assert_called_once_with('GET', '/')
 
     def test_shorten(self):
-        meta = {} # TODO
-        self.request.return_value = {'meta': meta, 'messages': [], 'success': True}
-        assert self.readability.shorten('some_url') == meta
-        self.request.assert_called_once_with('POST', '/urls',
-            headers={'Content-Type': 'application/x-www-form-urlencoded'},
-            data='url=some_url')
+        full_url = 'http://www.paulgraham.com/gh.html'
+        json = load_fixture('shorten.json')
+        self.request.return_value = json
+        assert self.readability.shorten(full_url) == json['meta']
+        self.request.assert_called_once_with('POST', '/urls', data='url=%s' % full_url,
+            headers={'Content-Type': 'application/x-www-form-urlencoded'})
 
     def test_metadata(self):
-        meta = {} # TODO
-        self.request.return_value = {'meta': meta, 'messages': [], 'success': True}
-        assert self.readability.metadata('some_id') == meta
-        self.request.assert_called_once_with('GET', '/urls/some_id')
+        url_id = 'ga4qf47t'
+        json = load_fixture('metadata.json')
+        self.request.return_value = json
+        assert self.readability.metadata(url_id) == json['meta']
+        self.request.assert_called_once_with('GET', '/urls/%s' % url_id)
 
 
 if __name__ == '__main__':
