74f3d8736d58e02ea154367b452c45c6f4135b46
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2012-12-19 13:27:01 +0100

    v0.2.0

--- rdd/__init__.py
+++ rdd/__init__.py
@@ -10,7 +10,7 @@ Python implementation of Readability Shortener API
 
 __author__ = 'Mathias Lafeldt <mathias.lafeldt@gmail.com>'
 __license__ = 'MIT'
-__version__ = '0.1.9'
+__version__ = '0.2.0'
 
 from .api import Readability
 from .exceptions import *
